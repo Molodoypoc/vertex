2db649efb1a213ca0e4be7e729c3cb12744cc70d
Molodoypoc <164372504+Molodoypoc@users.noreply.github.com>
2024-07-11 19:47:50 +0500

    Update Graph.h

--- Graph.h
+++ Graph.h
@@ -12,13 +12,13 @@ public:
     int getIndex(std::string name);
     int getEdgeWeight(std::string v1, std::string v2);
     void showFriends(int handshake);
-    void addUser(std::string name);// ���������� �������
-    void addEdge(std::string v1, std::string v2, int weight);// ���������� �����
+    void addUser(std::string name);
+    void addEdge(std::string v1, std::string v2, int weight);
 private:
     void findFriends(std::string start, std::string current, bool visited[], int handshake, int path);
-    bool edgeExists(std::string v1, std::string v2);// �������� ������������� �����
-    bool vertexExists(std::string name);// �������� ������������� �������
-    int matrix[SIZE][SIZE]; // ������� ���������
+    bool edgeExists(std::string v1, std::string v2);
+    bool vertexExists(std::string name);
+    int matrix[SIZE][SIZE];
     std::string vertexesName[SIZE];
-    int vertexCount; // ���������� ����������� ������
+    int vertexCount;
 };
